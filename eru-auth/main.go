3ad908dc43e1d6a5684dd9b3a1763c66ace0a0ed
abaradia1978 <abaradia@gmail.com>
2023-04-09 12:13:42 +0530

    fixes

--- eru-auth/main.go
+++ eru-auth/main.go
@@ -20,7 +20,7 @@ func main() {
 	logs.LogInit(server_handlers.ServerName)
 	logs.Logger.Info(fmt.Sprint("inside main of ", server_handlers.ServerName))
 
-	tp, err := eruotel.TracerInit()
+	tp, err := eruotel.TracerTempoInit()
 	if err != nil {
 		log.Fatal(err)
 	}
