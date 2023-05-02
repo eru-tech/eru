552a3289e5142632ff24cbfb289d1c11b6cc6f60
abaradia1978 <abaradia@gmail.com>
2023-05-02 17:13:52 +0530

    changes

--- eru-gateway/module_server/routes.go
+++ eru-gateway/module_server/routes.go
@@ -13,17 +13,17 @@ func SetServiceName() {
 }
 func AddModuleRoutes(serverRouter *mux.Router, sh *module_store.StoreHolder) {
 
+	//overwriting the handler of eru-server as gateway does not need variables and has to route to different services
+	serverRouter.Methods(http.MethodGet).Path("/{project}/variables/list").HandlerFunc(module_handlers.RouteHandler(sh.Store))
+	serverRouter.Methods(http.MethodPost).Path("/{project}/variables/savevar").HandlerFunc(module_handlers.RouteHandler(sh.Store))
+	serverRouter.Methods(http.MethodDelete).Path("/{project}/variables/removevar/{key}").HandlerFunc(module_handlers.RouteHandler(sh.Store))
+	serverRouter.Methods(http.MethodPost).Path("/{project}/variables/saveenvvar").HandlerFunc(module_handlers.RouteHandler(sh.Store))
+	serverRouter.Methods(http.MethodDelete).Path("/{project}/variables/removeenvvar/{key}").HandlerFunc(module_handlers.RouteHandler(sh.Store))
+	serverRouter.Methods(http.MethodPost).Path("/{project}/variables/savesecret").HandlerFunc(module_handlers.RouteHandler(sh.Store))
+	serverRouter.Methods(http.MethodDelete).Path("/{project}/variables/removesecret/{key}").HandlerFunc(module_handlers.RouteHandler(sh.Store))
+
 	//store routes specific to files
 	storeRouter := serverRouter.PathPrefix("/store").Subrouter()
-
-	//overwriting the handler of eru-server as gateway does not need variables and has to route to different services
-	storeRouter.Methods(http.MethodGet).Path("/{project}/variables/list").HandlerFunc(module_handlers.RouteHandler(sh.Store))
-	storeRouter.Methods(http.MethodPost).Path("/{project}/variables/savevar").HandlerFunc(module_handlers.RouteHandler(sh.Store))
-	storeRouter.Methods(http.MethodDelete).Path("/{project}/variables/removevar/{key}").HandlerFunc(module_handlers.RouteHandler(sh.Store))
-	storeRouter.Methods(http.MethodPost).Path("/{project}/variables/saveenvvar").HandlerFunc(module_handlers.RouteHandler(sh.Store))
-	storeRouter.Methods(http.MethodDelete).Path("/{project}/variables/removeenvvar/{key}").HandlerFunc(module_handlers.RouteHandler(sh.Store))
-	storeRouter.Methods(http.MethodPost).Path("/{project}/variables/savesecret").HandlerFunc(module_handlers.RouteHandler(sh.Store))
-	storeRouter.Methods(http.MethodDelete).Path("/{project}/variables/removesecret/{key}").HandlerFunc(module_handlers.RouteHandler(sh.Store))
 
 	storeRouter.Methods(http.MethodPost).Path("/listenerrule/compare").HandlerFunc(module_handlers.StoreCompareHandler(sh.Store))
 
