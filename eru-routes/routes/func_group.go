d52c8adb15d6d60527da605d1b83e31c67fb1af3
abaradia1978 <abaradia@gmail.com>
2023-10-19 09:52:29 +0530

    changes

--- eru-routes/routes/func_group.go
+++ eru-routes/routes/func_group.go
@@ -464,6 +464,7 @@ func (funcStep *FuncStep) transformRequest(ctx context.Context, request *http.Re
 		if oldContentType == multiPartForm {
 			//resetting it back to old content type as processMultipart will not be able to read the request body
 			req.Header.Set("Content-type", oldContentTypeFull)
+			vars.FormData, vars.FormDataKeyArray, vars.FileData, err = processMultipart(ctx, oldContentType, req, funcStep.RemoveParams.FormData, vars.FormData, vars.FileData)
 			if err != nil {
 				return
 			}
