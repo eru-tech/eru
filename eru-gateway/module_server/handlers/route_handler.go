17035cdf3c7484a407146ea715e8430353fa1f81
alty <abaradia@gmail.c>
2024-11-16 23:06:25 +0530

    api authentication

--- eru-gateway/module_server/handlers/route_handler.go
+++ eru-gateway/module_server/handlers/route_handler.go
@@ -45,7 +45,7 @@ func RouteHandler(s module_store.ModuleStoreI) http.HandlerFunc {
 			}
 			logs.WithContext(r.Context()).Info(token)
 			logs.WithContext(r.Context()).Info(r.Header.Get(authorizer.KidHeaderKey))
-			claims, err := authorizer.VerifyToken(r.Context(), r.Header.Get(authorizer.TokenHeaderKey), r.Header.Get(authorizer.KidHeaderKey))
+			claims, err := authorizer.VerifyToken(r.Context(), r.Header.Get(authorizer.TokenHeaderKey), fmt.Sprint("ERUAUTH_KID_", r.Header.Get(authorizer.KidHeaderKey)))
 			logs.WithContext(r.Context()).Info(fmt.Sprint(claims))
 			if err != nil {
 				server_handlers.FormatResponse(w, http.StatusUnauthorized)
