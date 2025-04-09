1853f41505e4674de3f0d22af4ca2d102c900ca3
alty <abaradia@gmail.c>
2025-04-09 23:19:14 +0530

    c

--- eru-utils/utils.go
+++ eru-utils/utils.go
@@ -222,13 +222,14 @@ func ExecuteHttp(ctx context.Context, req *http.Request) (resp *http.Response, e
 	}
 	PrintResponseBody(ctx, resp, "printing response immediately after utils.ExecuteHttp")
 
-	logs.WithContext(ctx).Info("callng deault transport http")
-	resp, err = http.DefaultTransport.RoundTrip(req)
-	if err != nil {
-		logs.WithContext(ctx).Error(err.Error())
+	if resp == nil {
+		logs.WithContext(ctx).Info("callng deault transport http")
+		resp, err = http.DefaultTransport.RoundTrip(req)
+		if err != nil {
+			logs.WithContext(ctx).Error(err.Error())
+		}
+		PrintResponseBody(ctx, resp, "printing response immediately after utils.ExecuteHttp")
 	}
-	PrintResponseBody(ctx, resp, "printing response immediately after utils.ExecuteHttp")
-
 	allowedOriginsI := ctx.Value("allowed_origins")
 	originI := ctx.Value("origin")
 
