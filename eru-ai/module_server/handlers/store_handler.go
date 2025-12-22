6f19a8ec0352797b527d5f06aa22abb5a7178810
alty <abaradia@gmail.c>
2025-12-22 15:32:25 +0530

    chnages

--- eru-ai/module_server/handlers/store_handler.go
+++ eru-ai/module_server/handlers/store_handler.go
@@ -863,6 +863,7 @@ func ToolWhatsAppEndpointExecuteHandler(sh *module_store.StoreHolder) http.Handl
 		logs.WithContext(r.Context()).Debug("ToolExecuteHandler - Start")
 		ctx := context.WithValue(r.Context(), "eruauthbaseurl", module_store.Eruauthbaseurl)
 		ctx = context.WithValue(ctx, "eruaiport", module_store.Eruaiport)
+		ctx = context.WithValue(ctx, tools.EruFuncBaseUrlKey, module_store.Erufuncbaseurl)
 		vars := mux.Vars(r)
 		projectId := vars["project"]
 		tenantId := vars["tenant"]
